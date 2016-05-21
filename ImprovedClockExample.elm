4a3ae775cb1562a1fb6d97363b931f3b9bace7f1
Mousaka <kristian.lundstrom@gmail.com>
2016-05-21 16:30:46 +0200

    Some refactoring done

--- ImprovedClockExample.elm
+++ ImprovedClockExample.elm
@@ -41,6 +41,7 @@ type Msg
   | Stop
   | Reset
   | Finished
+  | AddTime
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -61,6 +62,8 @@ update action model =
       ({model | finished = True}, Cmd.none)
     Reset ->
       init
+    AddTime ->
+      ({model | time = model.time + 10}, Cmd.none)
 
 -- SUBSCRIPTIONS
 
@@ -79,37 +82,43 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   let
-    secondsAngle =
-      angleHelper 60 model.time
-
-    minutesAngle =
-      angleHelper (60 * 60) model.time
-
-    secondsHandX =
-      toString (50 + 40 * cos secondsAngle)
-
-    secondsHandY =
-      toString (50 + 40 * sin secondsAngle)
-
-    minutesHandX =
-      toString (50 + 30 * cos minutesAngle)
-
-    minutesHandY =
-      toString (50 + 30 * sin minutesAngle)
-
     message = if model.finished then "Time is up!" else toString model.time
   in
     div [] [
-    svg [ viewBox "0 0 100 100", width "300px" ]
-        [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-        , line [ x1 "50", y1 "50", x2 minutesHandX, y2 minutesHandY, stroke "#000000" ] []
-        , line [ x1 "50", y1 "50", x2 secondsHandX, y2 secondsHandY, stroke "#F0F8FF" ] []
-        ]
+        clock model.time
         , div [] [text message]
         , div [] [button [ onClick Start ] [ text "Start" ]
               ,button [ onClick Stop ] [ text "Stop" ]
+              ,button [ onClick AddTime ] [ text "AddTime" ]
               ,button [ onClick Reset ] [ text "Reset timer" ]]
       ]
+
+clock : Float -> Html Msg
+clock time =
+    let
+      secondsAngle =
+        angleHelper 60 time
+
+      minutesAngle =
+        angleHelper (60 * 60) time
+
+      -- X Y coordinates for the handler tips
+      secondHandlerTip =
+        (50 + 40 * cos secondsAngle, 50 + 40 * sin secondsAngle)
+
+      minutesHandlerTip =
+        (50 + 30 * cos minutesAngle, 50 + 30 * sin minutesAngle)
+    in
+      svg [ viewBox "0 0 100 100", width "300px" ]
+          [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+          , clockHandle minutesHandlerTip "#000000"
+          , clockHandle secondHandlerTip "#F0F8FF"
+          ]
+
+clockHandle : (Float, Float) -> String -> Svg Msg
+clockHandle coords colour =
+  let (x, y) = coords in
+    line [ x1 "50", y1 "50", x2 (toString x), y2 (toString y), stroke colour ] []
 
 angleHelper : Float -> Float -> Float
 angleHelper speed seconds =
